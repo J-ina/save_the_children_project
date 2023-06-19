594be0ef69fc8bb5b64e64698c75cb4b3cc49898
Ina Jeon <junina93@gmail.com>
2023-06-19 09:43:02 +0900

    0619수정

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 $(function () {
-  // nav slide
+  // 풀다운 메뉴 이벤트
   $(".gnb").mouseover(function () {
     $(".gnb").find(".lnb").stop().slideDown();
     $(".gnb").next(".bg_lnb").stop().slideDown();
@@ -13,7 +13,7 @@ $(function () {
     $(".bg_lnb").stop().slideUp();
   })
 
-  // banner slide active
+  // 배너 마우스 이벤트
   const banner_box = $(".banner_wrap");
   const banner = $(".banner");
   let banner_timer = setInterval(banner_slide, 3000);
@@ -25,7 +25,27 @@ $(function () {
     banner_timer = setInterval(banner_slide, 3000);
   })
 
-  // banner title active
+  // 배너 이동 버튼 이벤트 (모바일)
+  const banner_prev = $(".prev_btn");
+  const banner_next = $(".next_btn");
+  banner_prev.click(function () {
+    clearInterval(banner_timer);
+    banner_current_index --;
+    if(banner_current_index < 0) {
+      banner_current_index = banner.length - 1;
+    }
+    banner_move(banner_current_index);
+  })
+  banner_next.click(function () {
+    clearInterval(banner_timer);
+    banner_current_index ++;
+    if(banner_current_index == banner.length) {
+      banner_current_index = 0;
+    }
+    banner_move(banner_current_index);
+  })
+
+  // 배너 타이틀 마우스 이벤트
   const banner_title = $(".banner_title > li");
   let banner_current_index = 0;
 
@@ -35,8 +55,11 @@ $(function () {
     banner_move(banner_current_index);
     clearInterval(banner_timer);
   })
-
-  // donate amount btn active
+  banner_title.mouseout(function () {
+    banner_timer = setInterval(banner_slide, 3000);
+  })
+  
+  // 후원 금액 클릭 이벤트
   const amount_value = [10000, 30000, 50000, 100000];
   const amount_btn = $(".amount_btn > button");
   const amount = $("#amount");
@@ -47,7 +70,7 @@ $(function () {
     amount.attr("value", `${amount_value[amount_btn_index]}`);
   })
 
-  // campaign list active
+  // 캠페인 리스트 마우스 이벤트
   const campaign_box = $("#campaign");
   const campaign_txt = $(".campaign_txt > li");
   const campaign_title = $(".campaign_title > li");
@@ -63,14 +86,14 @@ $(function () {
     );
   })
 
-  // youtube tab active
+  // 유튜브 탭 클릭 이벤트
   const video_title = $(".video_title");
 
   video_title.click(function () {
     on_active(video_title, $(this));
   })
 
-  // story img active
+  // 스토리 리스트 마우스 이벤트
   const story_list = $(".story_list > li");
   const story_img = $(".story_img");
   let story_current_index = 0;
@@ -83,35 +106,31 @@ $(function () {
     );
   })
 
-  // footer sites btn active
+  // 푸터 사이트 클릭 이벤트
   $(".sites_btn").click(function () {
     $(".sites_btn").next().stop().slideUp();
     $(this).next().stop().slideToggle();
   })
 
-  // on class function
+  // active 클래스 추가 function
   function on_active(items, e_item) {
-    items.removeClass("on");
-    e_item.addClass("on");
+    items.removeClass("active");
+    e_item.addClass("active");
   }
 
-  // banner slide init function
-  function banner_slide() {
-    let n = banner_current_index + 1;
-    if (n == banner.length) {
-      n = 0;
-      banner_current_index = 0;
-    }
-    banner_title.eq(n).trigger("mouseover");
-  }
-
-  // banner move function
+  // 배너 위치 이동 function
   function banner_move(i) {
     let banner_left = -100 * i;
-    if(i == 0) {
-      banner_box.stop().animate({ left: "0" }, 500);
-    } else {
-      banner_box.stop().animate({ left: `${banner_left}%` }, 500);
+    banner_box.stop().animate({ left: `${banner_left}%` }, 500);
+  }
+
+  // 배너 슬라이드 function
+  function banner_slide() {
+    banner_current_index ++;
+    if (banner_current_index == banner.length) {
+      banner_current_index = 0;
     }
+    on_active(banner_title, banner_title.eq(banner_current_index));
+    banner_move(banner_current_index);
   }
 })
